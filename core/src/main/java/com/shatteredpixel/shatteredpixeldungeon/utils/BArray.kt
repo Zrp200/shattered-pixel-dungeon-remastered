493192928ea4ef388905c85766ec4fc33b7610c5
Zachary Perlmutter <zrp200@gmail.com>
2022-08-29 12:11:33 -0700

    Added documentation to all used methods

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/utils/BArray.kt
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/utils/BArray.kt
@@ -24,19 +24,40 @@ package com.shatteredpixel.shatteredpixeldungeon.utils
 
 private var falseArray = booleanArrayOf()
 
-//This is MUCH faster than making a new boolean[] or using Arrays.fill;
+/**
+ * Sets every value in the [BooleanArray] to false.
+ *
+ * This is MUCH faster than making a new [boolean[]][BooleanArray] or using [Arrays.fill][java.util.Arrays.fill];
+ */
+
 fun BooleanArray.setFalse() {
     if (falseArray.size < size) falseArray = BooleanArray(size)
     falseArray.copyInto(this, endIndex = size)
 }
 
+/**
+ * Applies [Boolean.and] for each index in the BooleanArray and [b].
+ */
 infix fun BooleanArray.and(b: BooleanArray) = and(b, null)
 
+/**
+ * [Records][BooleanArray.populate] the result of applying [Boolean.and] for each index in the BooleanArray and [b].
+ * @param result The [BooleanArray] to overwrite.
+ */
 fun BooleanArray.and(b: BooleanArray, result: BooleanArray?) =
     result.populate(size) { this[it] && b[it] }
 
+/**
+ * Applies [Boolean.or] for each index in [BooleanArray] and [b].
+ */
 infix fun BooleanArray.or(b: BooleanArray) = or(b, result = null)
 
+/**
+ * [Records][BooleanArray.populate] the result of applying [Boolean.or] for each index in the BooleanArray and [b].
+ * @param offset the index to start the operation at
+ * @param length the amount of indices to apply the operation to
+ * @param result The [BooleanArray] to overwrite. Must have a [length][BooleanArray.size] of at least [offset]+[length].
+ */
 @JvmOverloads
 fun BooleanArray.or(
     b: BooleanArray,
@@ -45,11 +66,24 @@ fun BooleanArray.or(
     result: BooleanArray?
 ) = result.populate(length, offset) { get(it) || b[it] }
 
+/**
+ * inverts all values in the [BooleanArray].
+ */
 operator fun BooleanArray.not() = not(null)
+
+/**
+ * inverts all values in the [BooleanArray] and [records][BooleanArray.populate] it into [result]
+ */
 fun BooleanArray.not(result: BooleanArray?) = result.populate(size) { !this[it] }
 
+/**
+ * Returns a [BooleanArray] representing the indices where [v1] appears in the [IntArray]
+ */
 infix fun IntArray.`is`(v1: Int) = `is`(null, v1)
 
+/**
+ * [Records][BooleanArray.populate] representing all indices where [v1] appears in the [IntArray]
+ */
 fun IntArray.`is`(result: BooleanArray?, v1: Int) = mapInto(result, v1::equals)
 
 fun IntArray.isOneOf(result: BooleanArray?, vararg v: Int) = mapInto(result, v::contains)
@@ -61,16 +95,17 @@ fun IntArray.isNotOneOf(result: BooleanArray?, vararg v: Int) = mapInto(result) 
 // functions for mapping used in the above.
 
 /**
- * [IntArray.mapTo], but the result is a [BooleanArray] and the existing array is overwritten by the results of [predicate]
+ * [IntArray.mapTo], but the result is a [BooleanArray] and the [existing array][BooleanArray] is overwritten by the results of [predicate] if it already exists.
  **/
 private inline fun IntArray.mapInto(result: BooleanArray?, predicate: (value: Int) -> Boolean) =
     result.populate(size) { predicate(this[it]) }
 
 /**
- * overwrites a section of the BooleanArray using [predicate].
- * If there is no boolean array, one is created.
+ * overwrites a section of the [BooleanArray] using [predicate].
+ * If there is no [boolean array][BooleanArray], one is created.
  *
- * An [offset] can be optionally specified to only look at a portion of the arrays.
+ * @param offset index to start the operation at (leaving prior ones untouched).
+ * @param length amount of indices to operate on
  */
 private inline fun BooleanArray?.populate(
     length: Int,
